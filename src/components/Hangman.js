0d9993378309a44b62f0805045f7be5eac008cea
IrinaAlcala <58370677+IrinaAlcala@users.noreply.github.com>
2020-09-18 17:32:15 -0700

    initial start

---
+++ src/components/Hangman.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Hangman.css';
+import  { randomWord } from './Words.js';
+
+
+class Hangman extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hangman</h1>
+            </div>
+        )
+    }
+}
+
+export default Hangman;
